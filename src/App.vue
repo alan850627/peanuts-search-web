9551bc5b4f240081e8994c5b8115e767dd9da100
alan850627 <alan850627@gmail.com>
2016-06-25 11:57:19 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script>
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App'
+
+// Vue.use(SldsComponents)
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App },
+})
