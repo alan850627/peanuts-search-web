3971f17fe881f1a4c4f866d9e1efc3d2651648a4
Yi-Yang Chen <yachen@silabs.com>
2016-06-29 16:13:45 -0500

    Hello World!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="mainFrame">
-    <slds-page-header :lookup-text.sync="lookupText"></slds-page-header>
-    <slds-table :lookup-text="lookupText"></slds-table>
+    <!-- <slds-page-header :lookup-text.sync="lookupText"></slds-page-header>
+    <slds-table :lookup-text="lookupText"></slds-table> -->
+    Hello World!
   </div>
 </template>
 
